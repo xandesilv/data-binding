982d1dd333d61e72e0f273cc9a558321812543fa
Alexandre Silva <alexandre.b.silva@capgemini.com>
2022-10-17 07:49:07 -0300

    Data Binding

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.scss']
+})
+export class DataBindingComponent implements OnInit {
+
+  url: string = 'http://loiane.training';
+  cursoAngular: boolean = true ;
+  urlImagem = 'https://picsum.photos/id/237/200/300';
+  valorAtual: string = '';
+  valorSalvo: string = '';
+  isShown =  false;
+  isMouseOver = false;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  botaoClicado(){
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+
+  }
+
+  salvarValor(valor: any) {
+    this.valorSalvo = valor;
+    this.isShown = true;
+    setTimeout( () => {
+      this.isShown = false;}
+      , 3000 ,  );
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
